21d94bd80593a1ccaaedd892c126903e8dd10a8e
Kamil Marut <kamil@kamilmarut.com>
2021-10-21 21:09:00 +0200

    feat: Add change password

--- moonmen/views/auth.py
+++ moonmen/views/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, flash, redirect, render_template, request, session, url_for
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from moonmen.db import query_db
 from moonmen.decorators import login_required
@@ -30,7 +30,7 @@ def login():
         if "instance_id" in session:
             return redirect(url_for("index"))
         else:
-            return render_template("login.html")
+            return render_template("auth/login.html")
 
 
 @bp.route("/logout/", methods=["GET"])
@@ -38,3 +38,27 @@ def login():
 def logout():
     session.clear()
     return redirect(url_for("auth.login"))
+
+
+@bp.route("/change-password/", methods=["GET", "POST"])
+@login_required
+def change_password():
+    if request.method == "POST":
+        old_password = request.form.get("old_password", "")
+        new_password = request.form.get("new_password", "")
+        new_password_confirmation = request.form.get("new_password_confirmation", "")
+
+        instance = query_db("SELECT password FROM instances WHERE id = ?", [session["instance_id"]])[0]
+
+        if not check_password_hash(instance["password"], old_password):
+            flash("Old password is not valid")
+        elif new_password != new_password_confirmation:
+            flash("Password confirmation does not match the new password")
+        elif new_password == old_password:
+            flash("New password cannot be the same as old password")
+        else:
+            query_db("UPDATE instances SET password = ?", [generate_password_hash(new_password)], commit=True)
+            flash("Password changed successfully")
+            return redirect(url_for("index"))
+
+    return render_template("auth/change_password.html")
